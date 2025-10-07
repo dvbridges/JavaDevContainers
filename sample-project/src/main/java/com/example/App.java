6a19b7c57c73b95a7c737cf4751f230e25d2a282
David Bridges <david-bridges@hotmail.co.uk>
2025-10-07 23:59:34 +0100

    add project with devcontainers

---
+++ sample-project/src/main/java/com/example/App.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Running Java version: " + System.getProperty("java.version"));
+        int a = 5;
+        int b = 3;
+        int result = a * b;
+        System.out.println("Results: " + result);
+    }
+}
